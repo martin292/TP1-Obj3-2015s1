2a2a51430c0e96108d8156b3c1c97c27b8617f00
martin292 <martin.ramos292@gmail.com>
2015-04-09 16:25:26 -0300

    Completo test

--- src/main/scala/juego/TableroDeComando.scala
+++ src/main/scala/juego/TableroDeComando.scala
@@ -4,7 +4,7 @@ import scala.collection.mutable.ListBuffer
 
 trait TableroDeComando {
 
-  def personajes(): ListBuffer[Personaje]
+  def personajes(): ListBuffer[Personaje] 
   
   /**
    * Retorna cuántos personajes hay  
--- src/test/scala/Juego/TableroDeComandoSpec.scala
+++ src/test/scala/Juego/TableroDeComandoSpec.scala
@@ -5,12 +5,60 @@ import juego._
 
 class TableroDeComandoSpec extends FlatSpec with Matchers{
 
-  def massiveCheck(algo: TableroDeComando, cantP: Int, densidad: Int, medAtaque: Int, medDefensa: Int) = { }
+  def fixture =
+    new {
+      val jugador = new Jugador()
+      
+      val p1 = new Personaje(jugador, 30, 1, 30)
+      val p2 = new Personaje(jugador, 30, 1, 15)
+      val p3 = new Personaje(jugador, 30, 1, 0)
+      
+      jugador.personajes += p1
+      jugador.personajes += p2
+      jugador.personajes += p3
+      
+      val celda = new Rectangular()
+      
+      celda.personajes += p1
+      celda.personajes += p2
+      celda.personajes += p3
+      
+      val vecindad = new Vecindad()
+      
+      vecindad.personajes += p1
+      vecindad.personajes += p2
+      vecindad.personajes += p3
+  }
   
-  "una celda" should "hacer bien las cuentas" in{
-    val celda= new Rectangular()
-    massiveCheck(celda.vecindad,7,3,20,12)
+  
+  def chequeoMasivo(algo: TableroDeComando, cantP: Int, medAtaque: Int, medDefensa: Int) = { 
+    assert(algo.cantidadDePersonajes() == cantP)
+    assert(algo.medianaDeAtaque() == medAtaque)
+    assert(algo.medianaDeDefensa() == medDefensa)
+  }
+    
+  "ChequeoMasivo" should "Funcionar con jugador" in{
+    chequeoMasivo(fixture.jugador, 3, 13, 9)
+  }
+  
+  "ChequeoMasivo" should "Funcionar con celda" in{
+    chequeoMasivo(fixture.celda, 3, 13, 9)
+  }
+  
+  "ChequeoMasivo" should "Funcionar con vecindad" in{
+    chequeoMasivo(fixture.vecindad, 3, 13, 9)
+  }
+  
+  "PersonajeConMayorExperiencia" should "ser p1" in{
+    fixture.jugador.personajeConMayorExperiencia().experiencia should be (30)
+    fixture.celda.personajeConMayorExperiencia().experiencia should be (30)
+    fixture.vecindad.personajeConMayorExperiencia().experiencia should be (30)
   }
 
+  "personajesConLaDefensaMayorA" should "" in{
+    fixture.jugador.personajesConLaDefensaMayorA(9).size should be (1)
+    fixture.celda.personajesConLaDefensaMayorA(9).size should be (1)
+    fixture.vecindad.personajesConLaDefensaMayorA(9).size should be (1)
+  }
   
 }
