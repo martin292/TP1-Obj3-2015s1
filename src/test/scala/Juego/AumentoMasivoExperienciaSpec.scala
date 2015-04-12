6055b34ab65f7649808a62b463168f06eab741f8
martin292 <martin.ramos292@gmail.com>
2015-04-12 15:16:37 -0300

    Actualizo test aumento de experiencia

--- src/test/scala/Juego/AumentoMasivoExperienciaSpec.scala
+++ src/test/scala/Juego/AumentoMasivoExperienciaSpec.scala
@@ -8,28 +8,35 @@ class AumentoMasivoExperienciaSpec extends FlatSpec with Matchers{
   def fixture =
     new {
     
-      var jugador = new Jugador()
-      var celda = new Rectangular()
+      val jugador = new Jugador()
+      val celda = new Rectangular()
       
-      var p1 = new Personaje(jugador, 10, 0, 0)
-      var p2 = new Personaje(jugador, 1, 0, 0)
-      var p3 = new Personaje(jugador, 50, 0, 0)
+      val p1 = new Personaje(jugador, 10, 0, 0)
+      val p2 = new Personaje(jugador, 1, 0, 0)
+      val p3 = new Personaje(jugador, 50, 0, 0)
+      
+      val p4 = new Personaje(jugador, 10, 0, 0)
+      val p5 = new Personaje(jugador, 1, 0, 0)
+      val p6 = new Personaje(jugador, 50, 0, 0)
             
       jugador.personajes +=(p1)
       jugador.personajes +=(p2)
       jugador.personajes +=(p3)
       
-      celda.personajes +=(p1)
-      celda.personajes +=(p2)
-      celda.personajes +=(p3)      
+      celda.personajes +=(p4)
+      celda.personajes +=(p5)
+      celda.personajes +=(p6)      
       
-      var aumentadorJugador = new AumentoMasivoExperiencia(jugador)
-      var aumentadorCelda = new AumentoMasivoExperiencia(celda)
+      val aumentadorJugador = new AumentoMasivoExperiencia(jugador)
+      val aumentadorCelda = new AumentoMasivoExperiencia(celda)
+      
+      aumentadorJugador.ejecutar
+      aumentadorCelda.ejecutar
   }
   
   
   "Ejecutar con Jugador" should "aumentar experiencia de p1 y p3" in{
-    fixture.aumentadorJugador.ejecutar
+    
         
     fixture.aumentadorJugador.seEjecuto should be (true)
     
@@ -39,13 +46,13 @@ class AumentoMasivoExperienciaSpec extends FlatSpec with Matchers{
   }
   
   "Ejecutar con Celda" should "aumentar experiencia de p1 y p3" in{
-    fixture.aumentadorCelda.ejecutar
+    
         
     fixture.aumentadorCelda.seEjecuto should be (true)
     
-    fixture.p1.experiencia should be (4)
+    fixture.p1.experiencia should be (2)
     fixture.p2.experiencia should be (0)
-    fixture.p3.experiencia should be (4)
+    fixture.p3.experiencia should be (2)
   }
   
   
